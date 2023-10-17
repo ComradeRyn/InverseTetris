f676729bddcbcfc696285037fcd21dcfb512adc3
ComradeRyn <106786136+ComradeRyn@users.noreply.github.com>
2023-10-17 19:10:39 -0400

    Made blocks heavy

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -3,12 +3,12 @@ extends Node2D
 var block
 var spawnBlock = false
 var spawnRate = 1
-# Called when the node enters the scene tree for the first time.
+
 func _ready(): #Waits before spawning first block
 	await get_tree().create_timer(spawnRate).timeout
 	spawnBlock = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	if(spawnBlock): 
 		block = preload("res://Prefabs/I-Block.tscn").instantiate() #Replace with method that picks random location to spawn block
