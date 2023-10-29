4fa41f5cf92ca21197e0c55d092ba5c28f35d12b
ComradeRyn <106786136+ComradeRyn@users.noreply.github.com>
2023-10-29 18:00:38 -0400

    made the T

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -4,7 +4,7 @@ var block
 var spawnBlock = false
 var spawnRate = 1
 var decrementRate = 0.01
-var chosenSpot = -1
+var chosenSpot = -1 #location which the block will be placed
 
 func _ready(): #Waits before spawning first block
 	await get_tree().create_timer(spawnRate).timeout
@@ -13,9 +13,13 @@ func _ready(): #Waits before spawning first block
 
 func _process(delta):
 	if(spawnBlock): 
-		block = preload("res://Prefabs/I-Block.tscn").instantiate() #Replace with method that picks random location to spawn block
+		var randomBlock = randi_range(0,1) #random num that will be used to select type of block to place
+		if(randomBlock == 0):
+			block = preload("res://Prefabs/I-Block.tscn").instantiate() #Replace with method that picks random location to spawn block
+		if(randomBlock == 1):
+			block = preload("res://Prefabs/t-block.tscn").instantiate()
 		add_child(block)
-		if(block.chosenRot % 2 == 0):
+		if(block.chosenRot % 2 == 0): #selects the array which will have the position chosen from
 			chosenSpot = block.locationsDown[randi_range(0,block.locationsDown.size() - 1)]
 		else:
 			chosenSpot = block.locationsUp[randi_range(0,block.locationsUp.size() - 1)]
