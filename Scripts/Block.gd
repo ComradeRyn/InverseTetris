dd1b9cd4ab083fcc80276889903ee48f31b89e56
Vishnu <vishnurprasad1043@gmail.com>
2023-10-31 13:38:33 -0400

    blocks fall differently

--- Scripts/Block.gd
+++ Scripts/Block.gd
@@ -5,6 +5,8 @@ extends RigidBody2D
 @export var locationsUp : Array 
 var chosenRot = randi_range(0,3)
 
+var fallSpeed = 400
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
 	self.set_meta("type", "block")
@@ -16,9 +18,18 @@ func _ready():
 		self.set_rotation(PI)
 	else:
 		self.set_rotation(3 * PI/2)
+		
+	# Set an initial linear velocity in the y-direction
+	var initial_velocity = Vector2(0, fallSpeed)
+	set_linear_velocity(initial_velocity)
 
 
 func _process(delta):
+# Counteract gravity in the x-direction
+	var new_velocity = get_linear_velocity()
+	new_velocity.x = 0  # Set the x-component to 0 to lock the x-velocity
+	set_linear_velocity(new_velocity)
+	
 	var yVel = get_linear_velocity().y
 	if(yVel <= 0):
 		self.set_meta("block", "passive")
