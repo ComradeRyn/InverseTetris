6bd8409f5aa50192c601f3d094020a1b77c8f000
Vishnu <vishnurprasad1043@gmail.com>
2023-11-02 16:12:58 -0400

    added music node

---
+++ Music.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var chosenSong = randi_range(1,10)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if(chosenSong == 1):
+		$song1.play()
+	elif(chosenSong == 2):
+		$song2.play()
+	elif(chosenSong == 3):
+		$song3.play()
+	elif(chosenSong == 4):
+		$song4.play()
+	elif(chosenSong == 5):
+		$song5.play()
+	elif(chosenSong == 6):
+		$song6.play()
+	elif(chosenSong == 7):
+		$song7.play()
+	elif(chosenSong == 8):
+		$song8.play()
+	elif(chosenSong == 9):
+		$song9.play()
+	elif(chosenSong == 10):
+		$song10.play
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
