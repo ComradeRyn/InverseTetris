cd9f324e692ac400b635eae6eca05c772a84d3cc
SuperPupPresent <antfleck07@gmail.com>
2023-11-05 19:38:35 -0500

    Some weird stuff happened

--- Scripts/BlockStatic.gd
+++ Scripts/BlockStatic.gd
@@ -1,12 +1,14 @@
-extends StaticBody2D
+extends AnimatableBody2D
 
+const LIFETIME = 10
 
-@export var locationsDown : Array#[-160, -96, -32, 32, 96, 160]
-@export var locationsUp : Array #[-180, -160, -140, -120, -100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
+@export var locationsDown : Array #location arrays for the different oreientation of the blocks
+@export var locationsUp : Array 
 var chosenRot = randi_range(0,3)
+var time = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
+	self.set_meta("type", "block")
 	if(chosenRot == 0):
 		self.set_rotation(0)
 	elif(chosenRot == 1):
@@ -17,12 +19,9 @@ func _ready():
 		self.set_rotation(3 * PI/2)
 
 
-
-func _process(delta):
-	var yVel = get_linear_velocity().y
-	if(yVel <= 0):
-		self.set_meta("type", "passive")
-	else:
-		self.set_meta("type", "hostile")
-	await get_tree().create_timer(10).timeout
-	self.queue_free()
+func _physics_process(delta):
+	time += delta
+	self.set_meta("block", "hostile")
+	if(time >= LIFETIME):
+		self.queue_free()
+	
