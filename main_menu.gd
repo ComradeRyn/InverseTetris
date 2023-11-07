7675780938c3a8ab50a5167d70ea30505a081c8c
Vishnu <vishnurprasad1043@gmail.com>
2023-11-07 13:21:53 -0500

    button sfx

--- main_menu.gd
+++ main_menu.gd
@@ -4,8 +4,12 @@ func _ready():
 	$Song.play()
 
 func _on_quit_pressed():
+	$Bye.play()
+	await get_tree().create_timer(1.3).timeout
 	get_tree().quit()
 
 
 func _on_play_pressed():
+	$PlayGame.play()
+	await get_tree().create_timer(5).timeout
 	get_tree().change_scene_to_file("res://main_scene.tscn")
