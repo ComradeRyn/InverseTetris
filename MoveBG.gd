62d57bdacdbaaef27117e33d9b31048b74f34b50
Vishnu <vishnurprasad1043@gmail.com>
2023-11-15 12:02:36 -0500

    bg moves faster as time goes on

--- MoveBG.gd
+++ MoveBG.gd
@@ -1,6 +1,8 @@
 extends Sprite2D
 
 var speed = 0.25
+
+var incrementRate = .015
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,3 +13,9 @@ func _physics_process(delta):
 	if (position.y >= 108):
 		position.y = -178;
 	position.y += speed;
+	
+	var incrementAmount = incrementRate * delta #Ship moves faster as time goes on
+	speed += incrementAmount
+	print(speed)
+	if speed >= 2:
+		speed = 2
