40e3634ec18c269ad3be6fe2584bc2bd4585d5b6
Vishnu <vishnurprasad1043@gmail.com>
2023-11-15 15:29:48 -0500

    ship is animated now

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -4,6 +4,7 @@ extends TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.set_meta("type", "grid") # Replace with function body.
+	get_node("Ship").play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
