45e4be7bb85b88d1d5e80611c11a5890273993fc
ComradeRyn <106786136+ComradeRyn@users.noreply.github.com>
2023-11-28 20:13:00 -0500

    not to sure what this is lol

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -6,9 +6,9 @@ var numOfPlayers = 4
 func _process(delta):
 	if(Input.is_key_pressed(KEY_F11)):
 		swap_fullscreen_mode()
-#	if(numOfPlayers == 1): #win condition
-#		await get_tree().create_timer(1.5).timeout
-#		get_tree().change_scene_to_file("res://winner.tscn")
+	if(numOfPlayers == 1): #win condition
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_file("res://winner.tscn")
 	if(Input.is_action_just_pressed("restart") || numOfPlayers == 0):
 		await get_tree().create_timer(3).timeout
 		get_tree().reload_current_scene()
