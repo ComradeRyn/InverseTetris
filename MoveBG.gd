28253e9ca7dc3aa64db19af03ff747111da0526d
Vishnu <vishnurprasad1043@gmail.com>
2023-12-11 17:22:51 -0500

    moved instructions a little and made bg move slower

--- MoveBG.gd
+++ MoveBG.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var speed = 0.25
 
-var incrementRate = .03
+var incrementRate = .01
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
